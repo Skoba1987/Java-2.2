229894fef248d4f021cc5bdf55d99f8731a6cf1e
Skoba Kristina <105549745+Skoba1987@users.noreply.github.com>
2022-07-16 15:38:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ public class Main {
         System.out.println("Бонусы за пополнение!");
 
         int start = 500; // На счету абонента
-        int y = 100;
-        int x = 500; // Сумма пополнения
-        if (x >= 1000) {
+        int bonus = 100;
+        int add = 500; // Сумма пополнения
+        if (add >= 1000) {
             System.out.println("Поздравляем, Вам начислены бонусы!");
             System.out.println("Бонусов");
-            System.out.println(x / y);
+            System.out.println(add / bonus);
             System.out.println("Итого");
-            System.out.println((x / y) + start + x);
+            System.out.println((add / bonus) + start + add);
                                } else {
             System.out.println("Пополните на сумму от 1000 рублей и получи бонусы");
             System.out.println("Итого");
-            System.out.println(x + start);
+            System.out.println(add + start);
                     }
     }
 }
+
+
+
+
